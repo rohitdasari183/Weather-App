cb6aff4d63580012a6e6a45a8905e2255457f15f
rohitdasari183 <111290301+rohitdasari183@users.noreply.github.com>
2024-01-30 09:57:48 +0530

    Add files via upload

---
+++ weatherscript.js
@@ -0,0 +1,60 @@
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+const weather = {};
+weather.temperature = {
+    unit: "celcius"
+};
+const KELVIN = 273;
+const key = "9196ad745c6357ed868b6f545f73363e";
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser does not support geolocation</p>";
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error) {
+    notificationElement.innerHTML = `<p>${error.message}</p>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function () {
+            displayWeather();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function displayWeather() {
+    iconElement.innerHTML = `<img src="https://openweathermap.org/img/w/${weather.iconId}.png" width="200px"/>`;
+
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
